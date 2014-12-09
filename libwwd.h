79a22c91c817c5f4a680b007f0f3e79125f34e75
emanuelepesce <emanuele.pesce.ep@gmail.com>
2014-12-09 09:38:54 +0100

    Create libwwd.h

---
+++ libwwd.h
@@ -0,0 +1,20 @@
+/*
+* libwwd.h
+*/
+#ifndef LIBWWD_H_
+#define LIBWWD_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int getNumberOfCols(char* line, char* del);
+int getNumberOfRows(FILE *file);
+char *str_replace (const char *string, const char *substr, const char *replacement );
+double** loadData(FILE *file, int nCols, int nRows, char *del);
+double** readCSVFile(char* filename);
+int* getDatasetSize(char *filename);
+
+#endif /*libwwd.h*/
+
