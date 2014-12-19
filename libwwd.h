08ee79aa13d954420cf0242e40cf317bd39c6a84
Emanuele Pesce <emanuele.pesce.ep@gmail.com>
2014-12-19 20:58:41 +0100

    Added function 'showData'

--- libwwd.h
+++ libwwd.h
@@ -15,6 +15,7 @@ char *str_replace (const char *string, const char *substr, const char *replaceme
 double** loadData(FILE *file, int nCols, int nRows, char *del);
 double** readCSVFile(char* filename);
 int* getDatasetSize(char *filename);
+void showData(int nRows, int nCols, double** data);
 
 #endif /*libwwd.h*/
 
