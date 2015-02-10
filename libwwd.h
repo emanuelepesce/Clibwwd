ef9e83268392a631e2b01573fe742c4e707e76ca
Emanuele Pesce <emanuele.pesce.ep@gmail.com>
2015-02-10 21:21:09 +0100

    Added functions and changed documentation

--- libwwd.h
+++ libwwd.h
@@ -18,9 +18,12 @@ int* getDatasetSize(char *filename);
 void showData(int nRows, int nCols, double** data);
 double** loadSubsetData(FILE *file, int nCols, char *del, int *indices, int sizeI);
 double** readSubsetCSVFile(char* filename, int nRows, int nCols, int *indices);
-
+void showLowerMatrix(int nRows, double **data);
+void showDataInt(int nRows, int nCols, int** data);
+double** readSubsetCSVFileCol(char* filename, int nRows, int nCols, int *indices);
+double** loadSubsetDataCol(double** cmatrix, int nRows, int nCols, int *indices);
+double** transposeMat(double** mat, int nRows, int nCols);
 
 
 #endif /*libwwd.h*/
-
 
